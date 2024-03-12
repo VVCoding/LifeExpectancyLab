48fd6dea13aa0034860411e007649b2c574a3b91
Vedant Vohra <vohra.vedant@gmail.com>
2024-03-11 20:20:53 -0400

    FinishedtheLab

--- life_expectancy_lab/server.py
+++ life_expectancy_lab/server.py
@@ -57,12 +57,36 @@ def index():
     return render_template('index.html', all_years=all_years, cendpoints = endpoints("Canada"), mendpoints = endpoints("Mexico"), uendpoints = endpoints("UnitedStates"), avgval = avgval())
     
 
+def indexfinder(year):
+    f = open("data/life_expectancy.json", "r")
+    data = json.load(f)
+    f.close()
+    
+    index = {}
+    countries = list(data.keys())
+    
+    for country in countries:
+        if ((data[country][year])/((data["Mexico"][year]+data["UnitedStates"][year]+data["Canada"][year])/3)) <= 0.96:
+            index[country] = 0
+        elif ((data[country][year])/((data["Mexico"][year]+data["UnitedStates"][year]+data["Canada"][year])/3))  <= 0.98:
+            index[country] = 1
+        elif ((data[country][year])/((data["Mexico"][year]+data["UnitedStates"][year]+data["Canada"][year])/3))  <= 1.00:
+            index[country] = 2
+        elif ((data[country][year])/((data["Mexico"][year]+data["UnitedStates"][year]+data["Canada"][year])/3))  <= 1.02:
+            index[country] = 3
+        elif ((data[country][year])/((data["Mexico"][year]+data["UnitedStates"][year]+data["Canada"][year])/3))  >= 1.02:
+            index[country] = 4
+    
+    return index
+    
+    
 @app.route('/year')
 def year():
     f = open("data/life_expectancy.json", "r")
     data = json.load(f)
     f.close()
     
+    closeness = ["significantly lower than", "a little lower than", "about the same as", "a little higher than", "significantly higher than"]
     
     #check to see if year is in the query string portion of the URL
     requested_year = request.args.get('year')
@@ -76,15 +100,15 @@ def year():
     
     #represent all variables needed in render template
     # round values of life expectancy
-    roundedu = round(requested_data["UnitedStates"],2)
-    roundedc = round(requested_data["Canada"],2)  
-    roundedm = round(requested_data["Mexico"],2) 
+    roundedu = round(requested_data["UnitedStates"],1)
+    roundedc = round(requested_data["Canada"],1)  
+    roundedm = round(requested_data["Mexico"],1) 
     
     # rounded values of percent of North American Average
-    avgMex = round(((roundedm)/((roundedc+roundedm+roundedu)/3)*100),2)
-    avgUSA = round(((roundedu)/((roundedc+roundedm+roundedu)/3)*100),2) 
-    avgCanada = round(((roundedc)/((roundedc+roundedm+roundedu)/3)*100),2)  
+    avgMex = round(((roundedm)/((roundedc+roundedm+roundedu)/3)*100),1)
+    avgUSA = round(((roundedu)/((roundedc+roundedm+roundedu)/3)*100),1) 
+    avgCanada = round(((roundedc)/((roundedc+roundedm+roundedu)/3)*100),1)  
     
-    return render_template('year.html', requested_year= requested_year, data = requested_data, countries = countries, roundedu = roundedu, roundedm = roundedm, roundedc = roundedc, avgMex = avgMex, avgUSA = avgUSA, avgCanada = avgCanada)
+    return render_template('year.html', requested_year= requested_year, data = requested_data, countries = countries, roundedu = roundedu, roundedm = roundedm, roundedc = roundedc, avgMex = avgMex, avgUSA = avgUSA, avgCanada = avgCanada, closeness =closeness, index=indexfinder(requested_year))
 
 app.run(debug=True)
